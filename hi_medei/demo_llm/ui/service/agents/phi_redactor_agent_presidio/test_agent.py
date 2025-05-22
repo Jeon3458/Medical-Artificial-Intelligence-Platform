c7fff7f9dc25167e3d48fdf839032ed411a03fc2
Jeon3458 <gustjd3458@icloud.com>
2025-05-22 18:00:41 +0900

    Update test_agent.py

--- hi_medei/demo_llm/ui/service/agents/phi_redactor_agent_presidio/test_agent.py
+++ hi_medei/demo_llm/ui/service/agents/phi_redactor_agent_presidio/test_agent.py
@@ -1,8 +1,14 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from main import run
 
 input_text = {
-    "text": "전전현성은 2000년 6월 29일에 입원했고, 번호는 010-1234-5678입니다."
+    "text": "Hyunseong Jeon was admitted on June 29, 2000. His phone is 010-1234-5678."
 }
+
 
 result = run(input_text)
 print(result)
